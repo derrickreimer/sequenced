90628df540379c404acad6cea4a01d8246c37bea
Alejandro Mataloni <amataloni@gmail.com>
2021-04-26 10:30:20 -0300

    Set expiration for redis keys

--- lib/sequenced/generator.rb
+++ lib/sequenced/generator.rb
@@ -44,7 +44,7 @@ module Sequenced
       start_at = self.start_at.respond_to?(:call) ? self.start_at.call(record) : self.start_at
       last_id = find_last_record&.send(column) || 0
 
-      Redis.current.setnx(sequence_key, max(last_id, start_at - 1))
+      Redis.current.set(sequence_key, max(last_id, start_at - 1), nx: true, ex: 86400)
     end
 
     def next_id_in_sequence(increment:)
